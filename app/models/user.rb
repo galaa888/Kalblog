b6dedaef5a5169b3be541f9efeae65838b837527
Galbadrakh <axl.low15@gmail.com>
2017-03-24 14:33:31 -0500

    added association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save { self.email = email.downcase }
+
   validates :username, uniqueness: { case_sensetive: false },
             presence: true, length: { minimum: 2, maximum: 25 }
 
