4124bfaedee6802adb1d5b483f993eaed4057484
Galbadrakh <axl.low15@gmail.com>
2017-04-03 22:37:50 -0500

    Added full authentication system in both UI and Controller levels

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user  = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back " + user.username
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Sorry! There was something with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:warning] = "You have logged out"
+    redirect_to root_path
+  end
+
+end
